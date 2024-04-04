743b4d1a920df7577439732f0b4f8de5e85c68f0
Natasha Gandhi <natashagandhi02@gmail.com>
2024-04-04 12:50:31 -0400

    code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 data = pd.read_csv('vehicles_us.csv')
 
-st.header('Scatterplot of price vs. days listed with car type as color', divider='black')
+st.header('Scatterplot of price vs. days listed with car type as color')
 
 scatter2 = px.scatter(data, x='days_listed', y='price', color='type', title='Price by Days Listed with Car Type as Color',
                  labels={'days_listed': 'Days Listed', 'price': 'Price ($)', 'type': 'Type'})
@@ -15,7 +15,7 @@ st.plotly_chart(scatter2, use_container_width=True)
 
 
 
-st.header('Compare odometer (miles) distributions between car conditions', divider='black')
+st.header('Compare odometer (miles) distributions between car conditions')
 
 histogram1 = px.histogram(data, x='odometer', color='condition', title='Histogram of Odometer Miles with Condition as Color',
                    labels={'odometer': 'Odometer (miles)', 'condition': 'Condition'})
